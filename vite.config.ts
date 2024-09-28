696ceb9dd7d2d0cb4660011b268b8b6d90344666
rhammy <afaraeturahman@gmail.com>
2024-09-28 13:17:29 +0100

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import EnvironmentPlugin from "vite-plugin-environment";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), EnvironmentPlugin(["VITE_REACT_APP_API_URL"])],
-  build: {
-    outDir: "dist/app",
-  },
+
   server: {
     port: 5170,
   },
